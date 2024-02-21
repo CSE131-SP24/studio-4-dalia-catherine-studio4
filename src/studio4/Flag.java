b9a4b6106cdc240d2dd951b9958f84384b413eb3
daliahochstein <daliahochstein@localhost>
2024-02-21 15:10:30 -0600

    Japan flag final

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,16 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
-	}
+	StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
+}
 }
